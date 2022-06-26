ad94eea8084ceecd777e1298fbedf3b4807bbb55
User <https://github.com/arpifytech/in-fintech.git>
2022-06-26 22:12:03 +0400

    Portfolio

--- src/Services/Services.tsx
+++ src/Services/Services.tsx
@@ -1,10 +1,59 @@
 import React,{FC} from 'react'
+import {AiOutlineMobile} from 'react-icons/ai'
+import {FaLaptopCode} from 'react-icons/fa'
+import {HiDesktopComputer} from 'react-icons/hi'
+import {BsFileText} from 'react-icons/bs'
+import {FiTarget} from 'react-icons/fi'
+import {GrBrush} from 'react-icons/gr'
 import './Services.css'
 
 const Services:FC = () => {
     return(
-        <div className="services">
+        <div className="SERVICES">
+            <div className="SERVICESOur">
+                <p className="SERVICESOText">
+                    Our Services
+                </p>
+                <div className="crazy"></div>
 
+              <div className="APP">
+                  <div className="APPweb">
+                      <div className="APPm">
+                            <div className="UI">
+                                <AiOutlineMobile className="ai"/>
+                            </div>
+                      </div>
+                      <div className="APPm">
+                          <div className="UI">
+                              <FaLaptopCode className="ai"/>
+                          </div>
+                      </div>
+                      <div className="APPm">
+                          <div className="UI">
+                              <HiDesktopComputer className="ai"/>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="APPweb">
+                      <div className="APPm">
+                          <div className="UI">
+                               <BsFileText className="ai"/>
+                          </div>
+                      </div>
+                      <div className="APPm">
+                          <div className="UI">
+                               <FiTarget className="ai"/>
+                          </div>
+                      </div>
+                      <div className="APPm">
+                          <div className="UI">
+                               <GrBrush className="ai"/>
+                          </div>
+                      </div>
+                  </div>
+
+              </div>
+            </div>
         </div>
     )
 }
